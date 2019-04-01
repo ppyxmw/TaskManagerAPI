7ce39d718d3595ab77f9f8c773a7a1214dafec14
Michael <ppyxmw@gmail.com>
2019-04-01 18:24:47 +0100

    1q

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,9 +8,12 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
-
+  console.log('point 1')
   try {
+    console.log('point 2')
     await user.save()
+    console.log('point 3')
+
     sendWelcomeEmail(user.email, user.name)
     const token = await user.generateAuthToken()
     res.status(201).send({ user, token })
@@ -18,7 +21,6 @@ router.post('/users', async (req, res) => {
     res.status(400).send(e)
   }
 })
-
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
